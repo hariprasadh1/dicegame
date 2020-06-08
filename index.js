605152b07c7e6bfc9eb8d41e3ec91cab397b26af
hariprasadh1 <64316057+hariprasadh1@users.noreply.github.com>
2020-06-08 21:13:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const randomA=Math.floor(Math.random()*6)+1;
+const randomB=Math.floor(Math.random()*6)+1;
+randomADiceRoll="images/dice"+randomA+".png";
+randomBDiceRoll="images/dice"+randomB+".png";
+
+const die1=document.querySelector('.img1')
+die1.setAttribute("src",randomADiceRoll)
+
+let dice2=document.querySelector('.img2')
+
+dice2.setAttribute("src",randomBDiceRoll)
+console.log(dice2.getAttribute('images'))
+
+
+let results=document.querySelector(".hello")
+
+if(randomA>randomB)
+results.innerHTML="Player 1 Wins"
+else if(randomA < randomB)
+results.innerHTML="Player 2 Wins"
+else
+results.innerHTML="Draw"
